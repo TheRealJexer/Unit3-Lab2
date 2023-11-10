a04767ebfcc3c9014e216ab3ef89d88e749590d6
Bob <bob@gmail.com>
2023-11-10 14:48:41 -0700

    still stuck

--- firstTaskPart2.py
+++ firstTaskPart2.py
@@ -1,12 +1,11 @@
 # opens and reads the July9.csv file and gives it a TAB delimiter
-f_obj = open("July9.csv", mode = "r")
-data = f_obj.readline()
-items = data.split(",")
-weatherStation = str(items[3])
-year = (items[6])
-month = (items[7])
-for items in data:
-    print(weatherStation, year, month)
+with open("July9.csv") as f:
+    for line in f:
+        read = f.readline()
+        data = read.find()
+        print(data)
+
+
 
 
 
